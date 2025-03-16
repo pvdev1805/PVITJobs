39bffa8432e8392ae3cc026f050f662f4ba81116
Phu Vo <u1153426@umail.usq.edu.au>
2025-03-16 23:49:45 +1000

    📱 Implement Responsive - Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 tailwind.config = {
   theme: {
     screens: {
-      sm: "576px",
-      md: "768px",
-      lg: "992px",
-      xl: "1184px",
-      "2xl": "1184px",
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1216px',
+      '2xl': '1216px'
     },
     extend: {
-      colors: {},
-    },
-  },
-};
+      colors: {}
+    }
+  }
+}
