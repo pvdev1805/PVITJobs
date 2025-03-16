f43c478f27847f40fdf36f617eca37f3561e618b
Phu Vo <u1153426@umail.usq.edu.au>
2025-03-16 22:38:55 +1000

    🎉 Initialize project: Folder Structure

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1184px",
+      "2xl": "1184px",
+    },
+    extend: {
+      colors: {},
+    },
+  },
+};
